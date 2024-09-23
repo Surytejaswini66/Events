2c69474db1dc6a8946811817fb300ce74e5d5c30
Surytejaswini66 <suryaviswanadhapalli666@gmail.com>
2024-09-23 09:18:18 +0000

    solution

---
+++ src/components/EventItem/index.js
@@ -0,0 +1,24 @@
+// Write your code here
+// Write your code here
+import './index.css'
+
+const Event = props => {
+  const {eventDetails, changeEvent} = props
+  const {id, imageUrl, name, location} = eventDetails
+
+  const onChangeEvent = () => {
+    changeEvent(id)
+  }
+
+  return (
+    <li className="list-item">
+      <button type="button" className="event-button" onClick={onChangeEvent}>
+        <img src={imageUrl} alt="event" className="image" />
+      </button>
+      <p className="event-heading">{name}</p>
+      <p className="event-location">{location}</p>
+    </li>
+  )
+}
+
+export default Event
